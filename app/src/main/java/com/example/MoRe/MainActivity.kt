bdc5b7c1333db56a28dd3d2c037edbcdf88a686c
BRYAN-DESKTOP\bryan <bryanherberth@gmail.com>
2022-04-02 02:12:31 +0700

    Sign In page partly done

---
+++ app/src/main/java/com/example/MoRe/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.MoRe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+
+@Suppress("DEPRECATION")
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+
+        Handler().postDelayed({
+           val intent = Intent (this , SignIn::class.java)
+            startActivity(intent)
+        }, 3000)
+    }
+}
